34b86740f0638dd2705dae6b7d40f625f81ccd6c
jsc1013 <118318415+jsc1013@users.noreply.github.com>
2023-03-19 10:51:56 +0100

    Update SistemaLanzador.java
    Añadidos comentarios e instancia y lanzamiento de clase ActividadSalidaPuerta

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -16,9 +16,9 @@ public class SistemaLanzador {
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
-			// 
-			// TODO
-			//
+			// Creación de hilos de salida
+ 			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+ 			new Thread (salidas).start();
 			
 			
 		}
