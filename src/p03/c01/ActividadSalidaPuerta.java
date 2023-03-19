ec9d6093831b14523b35d715806f64fbabccda51
pac1006 <117029295+pac1006@users.noreply.github.com>
2023-03-19 09:57:03 +0000

    Update ActividadSalidaPuerta.java
    Se añaden librerías

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,5 +1,10 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ActividadSalidaPuerta implements Runnable{
 
 	private static final int NUMSALIDAS = 20;
